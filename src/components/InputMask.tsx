fccd7d651cc55c834320f47d376d2b87e15c73a2
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2022-07-27 22:50:10 -0300

    v2.3.0

--- src/components/InputMask.tsx
+++ src/components/InputMask.tsx
@@ -1,28 +1,34 @@
-import { MaskProps, useMask } from "mask-hooks";
+import { CompleteMask, MaskProps, useCompleteMask } from "mask-hooks";
 import { useEffect, useState } from "react";
 import { Form } from "react-bootstrap";
 
 export default function InputMask(props : MaskProps) {
 
-    const mask = useMask(props);
-    const [ value, setValue ] = useState<string>('');
-
-    function setValueMask(value : string) {
-        setValue(mask(value));
-    }
+    const maskComplete = useCompleteMask(props);
+    const [ maskData, setMaskData ] = useState<CompleteMask>(maskComplete(''));
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(() => setValueMask(''), [ props ]);
+    useEffect(() => setMaskData(maskComplete('')), [ props ]);
 
     return (
 
         <Form.Group>
+
             <Form.Label>Result</Form.Label>
+
             <Form.Control
                 title='Result'
-                value={ value }
-                onChange={ (input : any) => setValueMask(input.currentTarget.value) }
+                value={ maskData.result }
+                onChange={ (input : any) =>  setMaskData(maskComplete(input.currentTarget.value)) }
             />
+
+            <Form.Text className="text-white-50">
+                { Object.values(maskData).every(v => !!v)
+                    ? '✓ completed mask'
+                    : '✗ uncompleted mask'
+                }
+            </Form.Text>
+            
         </Form.Group>
 
     );
