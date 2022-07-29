9d712b9fd08f46b5df2a2fa15ead43c133f7c620
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2022-07-29 13:51:16 -0300

    force cursor to end of input when mask is infinity

--- src/components/InputMask.tsx
+++ src/components/InputMask.tsx
@@ -1,8 +1,10 @@
 import { CompleteMask, MaskProps, useCompleteMask } from "mask-hooks";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Form } from "react-bootstrap";
 
 export default function InputMask(props : MaskProps) {
+
+    const inputElement = useRef<HTMLInputElement>(null);
 
     const maskComplete = useCompleteMask(props);
     const [ maskData, setMaskData ] = useState<CompleteMask>(maskComplete(''));
@@ -17,9 +19,16 @@ export default function InputMask(props : MaskProps) {
             <Form.Label>Result</Form.Label>
 
             <Form.Control
+                ref={ inputElement }
                 title='Result'
                 value={ maskData.result }
-                onChange={ (input : any) =>  setMaskData(maskComplete(input.currentTarget.value)) }
+                onChange={ (input : any) =>  {
+                    setMaskData(maskComplete(input.currentTarget.value));
+                    if(props.infinity) {
+                        inputElement.current?.setSelectionRange(maskData.result.length + 1, maskData.result.length + 1);
+                        inputElement.current?.focus();
+                    }
+                } }
             />
 
             <Form.Text className="text-white-50">
