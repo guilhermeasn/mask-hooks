5a7a9c5e451ac144c9868c6a629bb4e2d3d959f7
Guilherme Augusto <guilhermeasn@yahoo.com.br>
2023-11-29 19:48:34 -0300

    updated and build

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -5,15 +5,26 @@ export default function Links() {
         <div className="m-3">
 
             <p>
+
                 GitHub:&nbsp;
                 <a href='https://github.com/guilhermeasn/mask-hooks' className="text-info" target='_blank' rel="noopener noreferrer">
                     https://github.com/guilhermeasn/mask-hooks
                 </a>
-            <br />
+
+                <br />
+
                 NPM:&nbsp;
                 <a href='https://www.npmjs.com/package/mask-hooks' className="text-info" target='_blank' rel="noopener noreferrer">
                     https://www.npmjs.com/package/mask-hooks
                 </a>
+
+                <br />
+
+                Sandbox:&nbsp;
+                <a href='https://codesandbox.io/p/sandbox/amazing-sea-2m3m8j' className="text-info" target='_blank' rel="noopener noreferrer">
+                    https://codesandbox.io/p/sandbox/amazing-sea-2m3m8j
+                </a>
+
             </p>
             
         </div>
